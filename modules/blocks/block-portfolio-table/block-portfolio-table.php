607a0c60ea2f273f35cd2ef3d90e42c26aa6a5e5
Jacint Virag <jacint.virag@brighterir.com>
2025-09-01 15:00:39 +0100

    portfolio table update

--- modules/blocks/block-portfolio-table/block-portfolio-table.php
+++ modules/blocks/block-portfolio-table/block-portfolio-table.php
@@ -20,11 +20,29 @@
     <?php else : ?>
         <div class="list-item">
     <?php endif; ?>
-            <img class="icon" src="<?php echo esc_url($icon); ?>" alt="<?php echo esc_attr($text); ?>">
-            <p class="company"><?php echo esc_html($company); ?></p>
-            <p class="first_invested"><span>First Invested</span><?php echo esc_html($first_invested); ?></p>
+            
+            <?php if($icon) : ?>
+            <div class="icon-wrapper">
+                <img class="icon" src="<?php echo esc_url($icon); ?>" alt="<?php echo esc_attr($text); ?>">
+                <p class="company-hover"><?php echo esc_html($company); ?></p>
+            </div>
+           <?php endif; ?>
+                 
+            <p class="company <?= ($icon) ? "" : 'no-icon'?>"><?php echo esc_html($company); ?></p>
+            
+            <p class="first_invested">
+                 <?php if($icon) : ?>
+                    <span>First Invested</span>
+                    <?php echo esc_html($first_invested); ?>
+                    <?php else : ?>
+                        <span class="no-value"></span>
+                    <?php endif; ?>
+            </p>
+
             <p class="value"><span>Value</span><?php echo esc_html($value); ?></p>
+            
      <?php if($url) : ?>
+        <span class="hover-link"></span>
         </a>
     <?php else : ?>
         </div>
